532670106fc381731de7a90c8690bc92fd8ccfc5
Dave Voutila <voutilad@gmail.com>
2023-05-30 13:11:45 -0400

    change to RichMapFunction to initialize logger

--- flink/src/main/java/com/redpanda/doom/fn/EchoFn.java
+++ flink/src/main/java/com/redpanda/doom/fn/EchoFn.java
@@ -1,14 +1,20 @@
 package com.redpanda.doom.fn;
 
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EchoFn<T> implements MapFunction<T, T> {
-  final transient Logger logger = LoggerFactory.getLogger(EchoFn.class);
+public class EchoFn<T> extends RichMapFunction<T, T> {
+  private transient Logger logger = null;
 
   @Override
-  public T map(T value) throws Exception {
+  public void open(Configuration parameters) {
+    logger = LoggerFactory.getLogger(this.getClass());
+  }
+
+  @Override
+  public T map(T value) {
     final String s = value == null ? "null" : value.toString();
     logger.info(s);
     return value;
